1dcd5bd19aec296e8a72a91aea5389caa8b02c50
Cuaritas <88283485+cuaritas@users.noreply.github.com>
2021-08-26 12:31:08 -0500

    Fix issue when amount  is 0

--- perfectlybalanced.sh
+++ perfectlybalanced.sh
@@ -105,7 +105,7 @@ rebalance () {
     echo $amount | grep -q "-"
     if [[ $? -eq 0 ]]; then
       reb -f $u2 -t $LIQUIDITY_DUMP --amount ${amount#-} --reckless --min-local 0 --min-amount 0 --fee-limit $MAX_FEE --min-remote 0
-    else
+    elif [[ $amount -ne "0" ]]; then
       reb -f $LIQUIDITY_PUMP -t $u2 --amount $amount --reckless --min-local 0 --min-amount 0 --fee-limit $MAX_FEE --min-remote 0
     fi
   done
